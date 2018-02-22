ec2d1780757c053cc4b863ab9fccd2b955b8eb7f
alika <alika.larsen@gmail.com>
2018-02-22 11:11:51 -0800

    Adding in more helm tests and adding a notes.txt

--- build/test.sh
+++ build/test.sh
@@ -1,15 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# This script installs the helm chart and tests
+# against the deployed helm release. This script
+# assumes helm is properly installed.
 
-[[ -z "$CHART_NAME"   ]] && \
-  {
-    echo >&2 "Var '$CHART_NAME' is empty. Cannot continue."
-    exit 1
-  }
+set -o errexit
+set -o nounset
+set -o pipefail
 
-[[ ! -d "${CHART_NAME}" ]] && \
-  {
-    echo >&2 "Directory for chart '$CHART_NAME' does not exist."
-    exit 1
-  }
+CHART_NAME=${CHART_NAME:?CHART_NAME must be set}
+NAMESPACE=${NAMESPACE:?NAMESPACE must be set}
+RELEASE=${RELEASE:?RELEASE must be set}
+INSTALL_WAIT=${WAIT:-120}
+
+if [[ ! -d ${CHART_NAME} ]]; then
+  echo >&2 "Directory for chart '$CHART_NAME' does not exist."
+  exit 1
+fi
 
 helm lint ${CHART_NAME}
+
+helm install --replace --name ${RELEASE} --namespace ${NAMESPACE} ./${CHART_NAME}
+
+# if there are tests, run them against the installed chart
+if [[ -d ${CHART_NAME}/templates/tests ]]; then
+  echo Testing release ${RELEASE}
+  helm test ${RELEASE} --cleanup
+  HELM_TEST_EXIT_CODE=$?
+fi
+
+exit ${HELM_TEST_EXIT_CODE:-0}
